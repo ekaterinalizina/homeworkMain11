060fe279527bc5640f43cffab944c5720a597c42
Nikolay Lavrenyuk <ekaterina.lizina87@gmail.com>
2021-01-19 20:39:17 -0500

    Add file HomeworkGit

---
+++ main/java/homeworkMain11/HomeworkGit.java
@@ -0,0 +1,10 @@
+package homeworkMain11;
+
+public class HomeworkGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am practicing GitHub");
+	}
+
+}
